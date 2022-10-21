98a45ea191fc01d71e1e4bdffd49ffc65bde2375
JuhaszSoma <soma.juhasz@stud.uni-corvinus.hu>
2022-10-21 11:05:44 +0200

    Webszolgáltatás hívása

--- Week5/Week5/Form1.cs
+++ Week5/Week5/Form1.cs
@@ -7,14 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Week5.MnbServiceReference;
+
 
 namespace Week5
 {
     public partial class Form1 : Form
     {
+        MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+        GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody()
+        {
+            currencyNames = "EUR",
+            startDate = "2020-01-01",
+            endDate = "2020-06-30"
+        };
+
+
         public Form1()
         {
             InitializeComponent();
+            var request = mnbService.GetExchangeRates(request);
+        }
+
+        public void GetExchangeRates()
+        {
+            
         }
     }
 }
