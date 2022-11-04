2c685b56e5fbed4286f328e62698d4568e9bb543
JuhaszSoma <soma.juhasz@stud.uni-corvinus.hu>
2022-11-04 10:43:27 +0100

    Labda létrehozása

---
+++ Week6/Week6/Entities/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week6
+{
+    public class Ball : Label
+    {
+
+        public Ball()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = Width;
+
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+
+        public void Move_Ball()
+        {
+            Left += 1;
+        }
+    }
+}
