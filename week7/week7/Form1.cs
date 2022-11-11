df12e8fac826f94e937fdd30d27348bb5f525493
JuhaszSoma <soma.juhasz@stud.uni-corvinus.hu>
2022-11-11 11:00:48 +0100

    8. Feladat + javít portfolia lista elmaradás

--- week7/week7/Form1.cs
+++ week7/week7/Form1.cs
@@ -23,10 +23,31 @@ namespace week7
             Ticks = context.Tick.ToList();
             dataGridView1.DataSource = Ticks;
             CreatePortfolio();
+            dataGridView2.DataSource = Portfolio;
+
+            List<decimal> Nyereségek = new List<decimal>();
+            int intervalum = 30;
+            DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
+            DateTime záróDátum = new DateTime(2016, 12, 30);
+            TimeSpan z = záróDátum - kezdőDátum;
+            for (int i = 0; i < z.Days - intervalum; i++)
+            {
+                decimal ny = GetPortfolioValue(kezdőDátum.AddDays(i + intervalum))
+                           - GetPortfolioValue(kezdőDátum.AddDays(i));
+                Nyereségek.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
+
+            var nyereségekRendezve = (from x in Nyereségek
+                                      orderby x
+                                      select x)
+                                        .ToList();
+            MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
         }
 
         private void CreatePortfolio()
         {
+            Portfolio = new List<PortfolioItem>();
             Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
             Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
             Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
