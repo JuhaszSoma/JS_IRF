286f5e7d998e5bed1b0df8e69e5a96a0c297481d
JuhaszSoma <soma.juhasz@stud.uni-corvinus.hu>
2022-11-11 10:44:41 +0100

    7. Feladat

--- week7/week7/Form1.cs
+++ week7/week7/Form1.cs
@@ -32,5 +32,21 @@ namespace week7
             Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
 
         }
+
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
+        }
+
     }
 }
