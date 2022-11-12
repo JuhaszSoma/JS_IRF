91f4f1439f4f873e3d6a7abf11bf1d6839fc4b16
TAMAS-PC\Tamas <tamas.rbskft@gmail.com>
2022-11-12 16:56:54 +0100

    Termékválasztó gomb

--- Week6/Week6/Form1.cs
+++ Week6/Week6/Form1.cs
@@ -16,11 +16,12 @@ namespace Week6
     {
         List<Toy> _toys = new List<Toy>();
         private IToyFactory _factory;
+        Toy _nextToy;
 
         public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set { _factory = value; DisplayNext(); }
         }
 
         public Form1()
@@ -56,6 +57,33 @@ namespace Week6
                 mainPanel.Controls.Remove(Torlendo);
             }
             
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+            {
+                Controls.Remove(_nextToy);
+            }
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = label1.Top + label1.Height + 20;
+            _nextToy.Left = label1.Left;
+            Controls.Add(_nextToy);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
