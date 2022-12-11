4f740d2cbd7e10ae642fef2cc6d219a4093ed861
TAMAS-PC\Tamas <tamas.rbskft@gmail.com>
2022-12-11 12:27:48 +0100

    Induló populáció

--- Week10/Week10/Form1.cs
+++ Week10/Week10/Form1.cs
@@ -15,13 +15,21 @@ namespace Week10
     {
         GameController gc = new GameController();
         GameArea ga;
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
         public Form1()
         {
             InitializeComponent();
 
             ga = gc.ActivateDisplay();
             Controls.Add(ga);
-            gc.AddPlayer();
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
             gc.Start(true);
         }
     }
